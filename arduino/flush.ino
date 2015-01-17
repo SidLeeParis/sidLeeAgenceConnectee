7e83b8a1f6ec3293099a23e210222bac28a08563
xseignard <xavier.seignard@gmail.com>
2015-01-17 22:48:25 +0100

    Updated flush code to work with the spark core

---
+++ arduino/flush.ino
@@ -0,0 +1,39 @@
+#include "HttpClient/HttpClient.h"
+
+#define PIN_SENSOR 2
+
+HttpClient http;
+
+http_header_t headers[] = {
+	{ "Content-Type", "application/x-www-form-urlencoded" },
+	{ NULL, NULL } // NOTE: Always terminate headers will NULL
+};
+
+http_request_t request;
+http_response_t response;
+
+void setup() {
+	Serial.begin(9600);
+	pinMode(PIN_SENSOR, INPUT_PULLUP);
+
+	request.hostname = "sidlee.herokuapp.com";
+	request.port = 80;
+	request.path = "/api/1/event";
+
+	delay(1000);
+}
+
+void loop() {
+	int sensorVal = digitalRead(2);
+	if (sensorVal == HIGH) {
+		sendEvent("flush", "1", "flush");
+		delay(10000);
+	}
+}
+
+void sendEvent(String name, String value, String unit) {
+	String data = "name=" + name + "&value=" + value + "&unit=" + unit;
+	Serial.println(data);
+	request.body = data;
+	http.post(request, response, headers);
+}
