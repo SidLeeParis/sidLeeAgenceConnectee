8771279a788b55c0bc072823f7ca139c9837dc1b
xseignard <xavier.seignard@gmail.com>
2015-01-20 16:23:09 +0100

    Updated client js

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 (function(SidLeeClient){
 	// create client
-	var client = new SidLeeClient('http://localhost:3000/', function(data) {
-	//var client = new SidLeeClient('https://sidlee.herokuapp.com/', function(data) {
+	//var client = new SidLeeClient('http://localhost:3000/', function(data) {
+	var client = new SidLeeClient('https://sidlee.herokuapp.com/', function(data) {
 		console.log(data);
 	});
 
