891601010a3dc69ea20a0e0b59ceefd6fa238fd2
xseignard <xavier.seignard@gmail.com>
2015-01-21 09:32:56 +0100

    removed some console logs

--- test/misc/facebookLikes.test.js
+++ test/misc/facebookLikes.test.js
@@ -5,9 +5,8 @@ var should = require('chai').should(),
 
 describe('getLikes', function() {
 
-	it('shoud return 5 likes', function(done) {
+	it('shoud return likes', function(done) {
 		getLikes(function(err, likes) {
-			console.log(likes);
 			should.not.exist(err);
 			likes._id.should.equal('likes');
 			likes.value.should.exist();
