80794671604df361cb8f613498f39bc5aa0f82d3
xseignard <xavier.seignard@gmail.com>
2015-01-26 00:25:26 +0100

    Using a mock library to mock http get
    - for facebook likes and visits

--- test/misc/facebookLikes.test.js
+++ test/misc/facebookLikes.test.js
@@ -1,15 +1,19 @@
 'use strict';
 /*jshint expr: true*/
 var should = require('chai').should(),
+	nock = require('nock'),
 	getLikes = require('../../src/misc/facebookLikes');
 
 describe('getLikes', function() {
 
 	it('shoud return likes', function(done) {
+		// mock http request
+		nock('http://graph.facebook.com').get('/203384865593').reply(200, {likes: 1000});
+
 		getLikes(function(err, likes) {
 			should.not.exist(err);
 			likes._id.should.equal('likes');
-			likes.value.should.exist();
+			likes.value.should.equal(1000);
 			done();
 		});
 	});
--- test/misc/googleAnalyticsVisits.test.js
+++ test/misc/googleAnalyticsVisits.test.js
@@ -1,15 +1,30 @@
 'use strict';
 /*jshint expr: true*/
 var should = require('chai').should(),
+	nock = require('nock'),
 	getVisits = require('../../src/misc/googleAnalyticsVisits');
 
 describe('getVisits', function() {
 
-	it('shoud return likes', function(done) {
-		getVisits(function(err, likes) {
+	it('shoud return visits', function(done) {
+		// mock http request
+		var date = new Date();
+		var start = date.getFullYear() + '-' + date.getMonth() +1 + '-' + date.getDate();
+		var end = start;
+		var fakeUrl = '/ga?ids=ga%3A96546222&start-date=' + start + '&end-date=' + end + '&metrics=ga%3Apageviews';
+		var fakeData = {
+			totalsForAllResults: {
+				'ga:pageviews': 15
+			}
+		};
+		nock('https://www.googleapis.com/analytics/v3/data')
+			.get(fakeUrl)
+			.reply(200, fakeData);
+
+		getVisits(function(err, visits) {
 			should.not.exist(err);
-			likes._id.should.equal('visits');
-			likes.value.should.exist();
+			visits._id.should.equal('visits');
+			visits.value.should.exist();
 			done();
 		});
 	});
