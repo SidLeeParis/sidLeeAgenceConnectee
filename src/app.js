2c4b9eb54b088d5a1be801a9e1a712be1a92e191
xseignard <xavier.seignard@gmail.com>
2015-01-29 18:43:05 +0100

    Dummy test

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ var express = require('express'),
 	getVisits = require('./misc/googleAnalyticsVisits'),
 	SensorsConf = require('./conf/sensorsConf');
 
+
 // connect to mongo
 mongoose.connect(conf.MONGO_URL);
 var db = mongoose.connection;
