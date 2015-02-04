65b7808d66ad2f7fb107994b5fd2fdcdac7362bf
xseignard <xavier.seignard@gmail.com>
2015-02-04 11:27:42 +0100

    Increasing apdex-t

--- src/newrelic.js
+++ src/newrelic.js
@@ -2,7 +2,7 @@ exports.config = {
   app_name : ['sidlee'],
   license_key : '04cea5d92ca19be513ed5d480097151e6e8363e6',
   // 900 ms of resp time, is considered ok for this particular app
-  apdex_t : 0.900,
+  apdex_t : 1.5,
   // ignore socket.io errors
   rules : {
     ignore : ['^/socket.io']
