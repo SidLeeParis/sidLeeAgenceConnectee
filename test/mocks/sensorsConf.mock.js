c313c9e0a684498e5af32c47323075720463c69f
xseignard <xavier.seignard@gmail.com>
2015-02-17 18:02:49 +0100

    Updated create route to track down lightswitch clicks

--- test/mocks/sensorsConf.mock.js
+++ test/mocks/sensorsConf.mock.js
@@ -1,6 +1,7 @@
 var SensorsConf = {
 	dummyName1: { name: 'dummyName1', sum: false },
-	undo: { name: 'undo', sum: true }
+	undo: { name: 'undo', sum: true },
+	lightswitch: { name: 'lightswitch', sum: true }
 };
 
 module.exports = SensorsConf;
