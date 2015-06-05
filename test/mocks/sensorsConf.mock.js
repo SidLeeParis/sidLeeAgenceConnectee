50fcbf3e0689cbf9e1da2bf576f5fe620725b4d7
xseignard <xavier.seignard@gmail.com>
2015-06-05 11:51:42 +0200

    Make test pass

--- test/mocks/sensorsConf.mock.js
+++ test/mocks/sensorsConf.mock.js
@@ -1,7 +1,8 @@
 var SensorsConf = {
 	dummyName1: { name: 'dummyName1', sum: false },
 	undo: { name: 'undo', sum: true },
-	lightswitch: { name: 'lightswitch', sum: true }
+	lightswitch: { name: 'lightswitch', sum: true },
+	watt: { name: 'watt', sum: false }
 };
 
 module.exports = SensorsConf;
